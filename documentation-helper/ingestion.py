3cfd88799a1b1d5886b5e45d604ece5c8a95f8e8
luciapuccini <puccinilucia@gmail.com>
2023-05-15 13:02:07 +0200

    load file as document

--- documentation-helper/ingestion.py
+++ documentation-helper/ingestion.py
@@ -5,18 +5,19 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 import pinecone
 
+print(os.environ["PINECONE_API_KEY"])
+
 pinecone.init(
     api_key=os.environ["PINECONE_API_KEY"],
     environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
 )
-
 
 def ingest_docs() -> None:
     loader = ReadTheDocsLoader(path="langchain-docs/langchain.readthedocs.io/en/latest")
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents) }documents")
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
+        chunk_size=400, chunk_overlap=50, separators=["\n\n", "\n", " ", ""]
     )
     documents = text_splitter.split_documents(documents=raw_documents)
     print(f"Splitted into {len(documents)} chunks")
